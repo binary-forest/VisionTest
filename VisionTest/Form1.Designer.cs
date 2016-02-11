19c1d0e7e8140fbf13b5e47053743f1a5f5d471c
James Potter <jackpott@hotmail.com>
2016-02-11 10:34:47 +0000

    Inital Code Base

---
+++ VisionTest/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace VisionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbMainImage = new System.Windows.Forms.PictureBox();
+            this.cbCameraList = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMainImage
+            // 
+            this.pbMainImage.Location = new System.Drawing.Point(12, 12);
+            this.pbMainImage.Name = "pbMainImage";
+            this.pbMainImage.Size = new System.Drawing.Size(452, 459);
+            this.pbMainImage.TabIndex = 0;
+            this.pbMainImage.TabStop = false;
+            // 
+            // cbCameraList
+            // 
+            this.cbCameraList.FormattingEnabled = true;
+            this.cbCameraList.Location = new System.Drawing.Point(778, 12);
+            this.cbCameraList.Name = "cbCameraList";
+            this.cbCameraList.Size = new System.Drawing.Size(201, 21);
+            this.cbCameraList.TabIndex = 1;
+            this.cbCameraList.SelectedIndexChanged += new System.EventHandler(this.cbCameraList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 485);
+            this.Controls.Add(this.cbCameraList);
+            this.Controls.Add(this.pbMainImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMainImage;
+        private System.Windows.Forms.ComboBox cbCameraList;
+    }
+}
+
---
+++ VisionTest/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+
+
+namespace VisionTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // =================================================================================
+        // Video effects.
+        // =================================================================================
+        #region Video effects
+        public static void DrawCrossFunct(ref Bitmap img, int x, int y)
+        {
+            Pen pen = new Pen(Color.Red, 1);
+            Graphics g = Graphics.FromImage(img);
+            g.DrawLine(pen, x / 2, 0, x / 2, y);
+            g.DrawLine(pen, 0, y / 2, x, y / 2);
+            pen.Dispose();
+            g.Dispose();
+        }
+
+        #endregion
+
+        private void cbCameraList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
